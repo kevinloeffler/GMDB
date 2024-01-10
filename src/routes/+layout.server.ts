9577309d0275588e347e09ae566f5844c3762c3e
Kevin Loeffler <hi@kevinloeffler.ch>
2024-01-10 20:01:51 +0100

    remove console log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async (event: any) => {
     const response = await event.fetch('/api/newest-movies')
-    console.log('response.ok:', response.ok)
     return {
         loggedIn: response.ok
     }
